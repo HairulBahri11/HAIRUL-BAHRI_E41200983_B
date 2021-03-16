bbd73cb8aab293546ad3000cc40d36cec37dd698
HairulBahri11 <80257426+HairulBahri11@users.noreply.github.com>
2021-03-16 19:12:45 +0700

    Create kesalahanvariabel.java.java

---
+++ TugasMinggu2/kesalahanvariabel.java.java
@@ -0,0 +1,31 @@
+
+package kesalahanvariabel.java;
+
+
+public class kesalahanvariabel {
+    
+    public static void main(String[] args) {
+        
+        
+        //SOAL dan Pembahasan
+        
+        boolean gameOver = false; //sudah benar
+        int student=50,classes=3; //sudah benar
+        double sales_tax; //sudah benar
+        short number1; //sudah benar
+        int 2beOrNot2be;  //salah, karena nama variabel tidak boleh diawali dengan angka
+        float price index; //salah, karena nama variabel tidak boleh dipisah , jika ingin dipisah di beri tanga ( _ )
+        double lastYear'sPrice; //salah karena nama variabel tidak boleh ada tanda ' (petik)
+        long class; // salah , karena tidak boleh menggunakan atribut yang sudah ada di aturan penulisan java
+        
+        
+        //Contoh Perbaikan
+                
+        int twobeOrNotTwobe;
+        float price_index;
+        double lastYearsPrice;
+        long classer;
+                
+    }
+    
+}
