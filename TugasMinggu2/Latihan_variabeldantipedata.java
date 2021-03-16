42c8453f9e990e30c1e6d7601a41a131ed531232
HairulBahri11 <80257426+HairulBahri11@users.noreply.github.com>
2021-03-16 19:13:01 +0700

    Create Latihan_variabeldantipedata.java

---
+++ TugasMinggu2/Latihan_variabeldantipedata.java
@@ -0,0 +1,17 @@
+
+package Latihan_variabeldantipedata;
+
+
+public class Latihan_variabeldantipedata {
+    
+    public static void main(String[] args) {
+        String nama = "Indonesia";
+        char abjad = 'C';
+        int a = 6;
+        double b = 6.9;
+        
+        System.out.println("nama");
+        System.out.println(a);
+    }
+    
+}
