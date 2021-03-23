b56b171c93618f27850b90735f5094d6320413f9
HairulBahri11 <80257426+HairulBahri11@users.noreply.github.com>
2021-03-23 18:43:36 +0700

    Create Latihan_Array.java

---
+++ TugasMinggu3/Latihan_Array.java
@@ -0,0 +1,15 @@
+
+package latihan_array;
+
+
+public class Latihan_Array {
+
+    public static void main(String[] args) {
+       
+        // Menghitung jumlah elemen array
+        
+        int[] angka = {3,7,8,1,10,20,35};
+        
+        System.out.println("Jumlah elemen array angka = " +angka.length);
+        }
+}
