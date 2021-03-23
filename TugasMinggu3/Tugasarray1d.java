cb594352e46d6f219df5457bd7c0ce693a86b668
HairulBahri11 <80257426+HairulBahri11@users.noreply.github.com>
2021-03-23 18:43:19 +0700

    Create Tugasarray1d.java

---
+++ TugasMinggu3/Tugasarray1d.java
@@ -0,0 +1,20 @@
+
+package tugasarray1d;
+
+
+public class Tugasarray1d {
+
+ 
+    public static void main(String[] args) {
+    
+        
+       String[] nama = new String[2];
+       nama[0] = "HAIRUL";
+       nama[1] = "BAHRI";
+       String namaLengkap = nama[0] + " " + nama[1];
+       
+        
+        System.out.println(" NAMA = " + namaLengkap);
+    }
+    
+}
