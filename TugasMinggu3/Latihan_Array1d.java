967c0e8112465ff6928c7b787cc41b00809163ad
HairulBahri11 <80257426+HairulBahri11@users.noreply.github.com>
2021-03-23 18:43:33 +0700

    Create Latihan_Array1d.java

---
+++ TugasMinggu3/Latihan_Array1d.java
@@ -0,0 +1,23 @@
+
+package latihan_array1d;
+
+
+public class Latihan_Array1d {
+
+
+    public static void main(String[] args) {
+      
+        int [] angka = { 5,10,17,20,1 };
+        String [] teks = { "Saya", " Sedang", " Belajar", "OOP", "Java"};
+        
+        for (int i=0; i<angka.length; i++){
+            System.out.println(angka[i]);
+        }
+        
+        for ( int j=0; j<teks.length; j++){
+            System.out.print(teks[j]);
+            System.out.print(" ");
+    }
+    
+}
+}
