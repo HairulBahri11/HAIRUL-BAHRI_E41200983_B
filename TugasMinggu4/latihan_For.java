f27539a306790cc2e30bc054ba130f180a9018b0
HairulBahri11 <80257426+HairulBahri11@users.noreply.github.com>
2021-03-30 19:19:24 +0700

    Create latihan_For.java

---
+++ TugasMinggu4/latihan_For.java
@@ -0,0 +1,17 @@
+
+package wdsi;
+
+
+public class latihan_For {
+    
+    
+    public static void main(String[] args) {
+        
+        for (int  i = 0 ; i<= 5 ; i++){
+            for( int j = 0; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");    
+        }
+    }
+}
