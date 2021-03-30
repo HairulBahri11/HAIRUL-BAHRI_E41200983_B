5755daee9aa52957d169285d3765fa5c677ca1fa
HairulBahri11 <80257426+HairulBahri11@users.noreply.github.com>
2021-03-30 19:19:17 +0700

    Create latihan_Foreach.java

---
+++ TugasMinggu4/latihan_Foreach.java
@@ -0,0 +1,18 @@
+
+package wdsi;
+
+
+public class latihan_Foreach {
+    
+    
+    public static void main(String[] args) {
+        
+        //membuat array
+        int angka[] = {3,1,42,24,12};
+        
+        //menggunakan perulangan for each untuk menampilkan angka
+        for( int x : angka){
+            System.out.print(x +" ");
+        }
+    }
+}
