02f89af0bbe12c19f1b1dfd7d9538861b141ba4d
HairulBahri11 <80257426+HairulBahri11@users.noreply.github.com>
2021-03-30 19:19:13 +0700

    Create latihan_While.java

---
+++ TugasMinggu4/latihan_While.java
@@ -0,0 +1,23 @@
+
+package wdsi;
+
+/**
+ *
+ * @author user
+ */
+public class latihan_While {
+    
+    public static void main(String[] args) {
+        
+      int i = 0;
+      while ( i <= 10){
+          
+          //blok kode yang akan diulang
+          System.out.println("Perulangan ke-" + i);
+          
+          //increment nilai i
+          i++;
+      }
+      
+    }
+}
