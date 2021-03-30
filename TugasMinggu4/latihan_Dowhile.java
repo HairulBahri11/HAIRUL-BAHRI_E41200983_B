d5585b462970015b2b39c0c5af2c67259f8f4696
HairulBahri11 <80257426+HairulBahri11@users.noreply.github.com>
2021-03-30 19:18:53 +0700

    Create latihan_Dowhile.java

---
+++ TugasMinggu4/latihan_Dowhile.java
@@ -0,0 +1,17 @@
+
+package wdsi;
+
+
+public class latihan_Dowhile {
+    
+    public static void main(String[] args) {
+        
+        int i =0;
+        
+        do {
+            System.out.println("Perulangan ke-" + i);
+            i++;
+        }while ( i<=10);
+    }
+    
+}
