1adfe1bf0850bd155dc5a587edb4bb6860d84682
HairulBahri11 <80257426+HairulBahri11@users.noreply.github.com>
2021-04-04 16:40:19 +0700

    Create tugasPerkalian2danKelipatan2.java

---
+++ TugasMinggu4/tugasPerkalian2danKelipatan2.java
@@ -0,0 +1,43 @@
+
+package wdsi;
+
+
+public class tugasKelipatan2 {
+    
+    public static void main(String[] args) {
+        System.out.println("              Do_While               ");
+        System.out.println("    Bilangan perkalian 2 ( 0-100)");
+        System.out.println("======================================");
+ 
+        //tugas kelipatan 2
+ int angkaAwal = 0;
+ angkaAwal= angkaAwal+2;
+ int batasAkhir =100;
+   do {
+       
+      
+        System.out.print(angkaAwal);
+       angkaAwal= angkaAwal * 2;
+       System.out.print("    ");
+       
+       
+   }while( angkaAwal<=batasAkhir);
+   
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println("              FOR_LOOP               ");
+        System.out.println("    Bilangan kelipatan  2 ( 0-100)");
+        System.out.println("====================================");
+        
+            angkaAwal= 2;
+            batasAkhir = 50;
+        for( int i = 1; i<= batasAkhir; i++){
+            
+            System.out.println(i + " x  2 =  " +angkaAwal );
+            angkaAwal = angkaAwal+2;
+        }
+       }
+    
+   
+    
+}
