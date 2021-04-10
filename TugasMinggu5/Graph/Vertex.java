232958681c6c5a3d4b8275cb6f8453b90b7162f8
HairulBahri11 <80257426+HairulBahri11@users.noreply.github.com>
2021-04-10 18:52:45 +0700

    Create Vertex.java

---
+++ TugasMinggu5/Graph/Vertex.java
@@ -0,0 +1,14 @@
+
+package wdsi;
+
+
+public class Vertex {
+    
+    public char label;
+    public boolean  wasVisited;
+    
+    public Vertex ( char label){
+        this.label = label;  
+        wasVisited = false;
+    }
+}
