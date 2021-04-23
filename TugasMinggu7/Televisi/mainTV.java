491332cff933cc4530364355b9ce0c4209dffc7e
HairulBahri11 <80257426+HairulBahri11@users.noreply.github.com>
2021-04-23 23:04:59 +0700

    Create mainTV.java

---
+++ TugasMinggu7/Televisi/mainTV.java
@@ -0,0 +1,34 @@
+
+package Minggu7;
+
+
+ //  author :  Hairul Bahri
+ 
+public class mainTV {
+    
+    
+    public static void main(String[] args) {
+        
+       Televisi tv = new Televisi ("TV Tempoe Doeloe, 14 inches", 10);
+        
+       String [] semuaChannel = {"RCTI" , "SCTV", "INDOSIAR", "LA TV","METRO TV"
+                                                ,"METRO TV", "TRANS TV", "TPI", "TV7", "TVRI"
+                                                 ,"TV G", "AN TV"};
+       String [] channelFavorit = { "RCTI" , "SCTV", "INDOSIAR", "LA TV","METRO TV"
+                                                ,"METRO TV", "TRANS TV", "TPI", "TV7"};
+    
+        System.out.println("Pak Lukman, membeli Televisi : " + tv.getDeskripsi());
+       
+        tv.getChannels();
+        tv.setChannels(semuaChannel);
+        tv.setChannels(channelFavorit);
+        
+        tv.setChannelAktif(3);
+        tv.setVolume(10);
+        tv.setChannelAktif(10);
+        tv.setChannelAktif(1);
+     
+    } 
+    
+    }
+
